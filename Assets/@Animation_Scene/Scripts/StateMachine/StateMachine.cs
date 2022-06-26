091085506c8bb2f88c4532e53c578308a23cc041
boss.timysn94@outlook.com <boss.timysn94@outlook.com>
2022-06-26 22:30:20 +0300

    - Новая стейт машина и работа с анимациями.

---
+++ Assets/@Animation_Scene/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ActionCatGame.Core.State
+{
+    public class StateMachine : MonoBehaviour
+    {
+        protected IState CurrentState;
+
+        public void ChangeState(IState newState) 
+        {
+            CurrentState?.Exit();
+            CurrentState = newState; 
+            CurrentState?.Enter();
+        }
+
+        public void HandleInput()
+        {
+            CurrentState?.HandleInput();
+        }
+        public void Update()
+        {
+            CurrentState?.Update();
+        }
+        public void PhysicsUpdate()
+        {
+            CurrentState?.PhysicsUpdate();
+        }
+    }
+}
